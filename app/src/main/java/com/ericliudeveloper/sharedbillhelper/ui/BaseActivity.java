6f7e302852b2bf1db4f45deffa53e43b3aea0e75
Eric Liu <eric.liu.developer@gmail.com>
2015-06-07 00:26:04 +1000

    creates activity_details.xml
    Experimenting with Material Design Library

---
+++ app/src/main/java/com/ericliudeveloper/sharedbillhelper/ui/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.ericliudeveloper.sharedbillhelper.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by liu on 6/06/15.
+ */
+public class BaseActivity extends AppCompatActivity {
+}
