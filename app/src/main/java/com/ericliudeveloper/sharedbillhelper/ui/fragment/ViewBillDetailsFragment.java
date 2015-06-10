dd6ab2a4ded5dd59be3cdebb49b97b9e1772600a
Eric Liu <eric.liu.developer@gmail.com>
2015-06-10 13:05:49 +1000

    * Add event listeners to ViewBillDetailsFragment *

--- app/src/main/java/com/ericliudeveloper/sharedbillhelper/ui/fragment/ViewBillDetailsFragment.java
+++ app/src/main/java/com/ericliudeveloper/sharedbillhelper/ui/fragment/ViewBillDetailsFragment.java
@@ -1,18 +1,25 @@
 package com.ericliudeveloper.sharedbillhelper.ui.fragment;
 
 
-import android.os.Bundle;
+import android.app.Activity;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.ericliudeveloper.sharedbillhelper.R;
+import com.ericliudeveloper.sharedbillhelper.model.Bill;
+import com.ericliudeveloper.sharedbillhelper.util.CustomEvents;
+
+import de.greenrobot.event.EventBus;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ViewBillDetailsFragment extends Fragment {
+    Bill mBill;
 
 
     public ViewBillDetailsFragment() {
@@ -28,4 +35,21 @@ public class ViewBillDetailsFragment extends Fragment {
     }
 
 
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        EventBus.getDefault().registerSticky(this);
+    }
+
+    public void onEvent(CustomEvents.EventViewBill eventViewBill) {
+        mBill = eventViewBill.bill;
+        Toast.makeText(getActivity(), "AMount : " + mBill.getAmount(), Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        EventBus.getDefault().unregister(this);
+    }
 }
