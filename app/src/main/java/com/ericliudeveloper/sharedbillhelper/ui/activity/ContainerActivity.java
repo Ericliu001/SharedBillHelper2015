bdd921d9daf3a9510a62b920b092db56f24fc744
Eric Liu <eric.liu.developer@gmail.com>
2015-06-10 10:28:49 +1000

    * changes ContainerActivity to allow subclasses to decide which layout to inflate * Let ViewBillDetailsActivity subclass ContainerActivity * create a FrameLayout with id/container in layout_detail.xml file * The ViewBillDetailsFragment is injected into the detail view programmacally

--- app/src/main/java/com/ericliudeveloper/sharedbillhelper/ui/activity/ContainerActivity.java
+++ app/src/main/java/com/ericliudeveloper/sharedbillhelper/ui/activity/ContainerActivity.java
@@ -4,27 +4,50 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.ericliudeveloper.sharedbillhelper.R;
 
 /**
  * Created by liu on 8/06/15.
  */
-public abstract class ContainerActivity extends AppCompatActivity{
+public abstract class ContainerActivity extends AppCompatActivity {
 
     protected abstract Fragment getFragment();
+
     protected abstract String getFragmentTag();
+
+    /**
+     * In case the subclass wants to use a different layout xml file,
+     * the child class can override this method to supply the layout resource id
+     *
+     * @return
+     */
+    protected int getChildLayoutResID() {
+        return -1;
+    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.empty_container);
+        int layoutResID = getChildLayoutResID();
+        if (layoutResID > 0) {
+            setContentView(layoutResID);
+        } else {
+            setContentView(R.layout.empty_container);
+        }
 
-        String tag = getFragmentTag();
-        FragmentManager fm = getFragmentManager();
-        if (fm.findFragmentByTag(tag) == null) {
-            Fragment frag = getFragment();
-            fm.beginTransaction().add(R.id.container, frag, tag).commit();
+        View container = findViewById(R.id.container);
+        if (container != null) {
+
+            String tag = getFragmentTag();
+            FragmentManager fm = getFragmentManager();
+            if (fm.findFragmentByTag(tag) == null) {
+                Fragment frag = getFragment();
+                fm.beginTransaction().add(R.id.container, frag, tag).commit();
+            }
         }
     }
+
 }
--- app/src/main/java/com/ericliudeveloper/sharedbillhelper/ui/activity/ViewBillDetailsActivity.java
+++ app/src/main/java/com/ericliudeveloper/sharedbillhelper/ui/activity/ViewBillDetailsActivity.java
@@ -2,10 +2,16 @@ package com.ericliudeveloper.sharedbillhelper.ui.activity;
 
 import android.app.Fragment;
 
+import com.ericliudeveloper.sharedbillhelper.R;
 import com.ericliudeveloper.sharedbillhelper.ui.fragment.ViewBillDetailsFragment;
 
 public class ViewBillDetailsActivity extends ContainerActivity {
 
+
+    @Override
+    protected int getChildLayoutResID() {
+        return R.layout.layout_detail;
+    }
 
     @Override
     protected Fragment getFragment() {
